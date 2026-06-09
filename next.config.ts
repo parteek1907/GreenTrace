a36228cf97c25884be47f160ca0779b4c171bdb1
Parteek Garg <gargparteek1907@gmail.com>
2026-06-09 20:24:37 +0530

    chore: Update next.config.ts to allow HMR over local network IPs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["192.168.31.116", "localhost", "127.0.0.1"],
 };
 
 export default nextConfig;
