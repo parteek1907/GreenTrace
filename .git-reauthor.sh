964b393fac101dafa9c4bc40a56ff77141d72773
Parteek Garg <gargparteek1907@gmail.com>
2026-06-10 14:06:30 +0530

    chore: remove AI artifacts and project planning documentation

--- .git-reauthor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git filter-branch -f --env-filter '
-if [ "$GIT_AUTHOR_EMAIL" = "bot@greentrace.app" ]; then
-    export GIT_AUTHOR_NAME="Parteek Garg"
-    export GIT_AUTHOR_EMAIL="gargparteek1907@gmail.com"
-    export GIT_COMMITTER_NAME="Parteek Garg"
-    export GIT_COMMITTER_EMAIL="gargparteek1907@gmail.com"
-fi
-' HEAD~25..HEAD
